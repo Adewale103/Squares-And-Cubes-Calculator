ccccf952fac852282fdd84c9ffe57760b507f8c7
Adewale Adeyinka <98183852+Adewale103@users.noreply.github.com>
2022-02-25 17:07:07 +0100

    Add files via upload

---
+++ TableOfSquaresAndCubes.java
@@ -0,0 +1,42 @@
+public class TableOfSquaresAndCubes{
+	public static void main(String[] args){
+	
+	int number;
+
+	System.out.printf("Number Square Cube%n");
+
+	number = 0;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+	
+	number = 1;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+
+	number = 2;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+
+	number = 3;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+
+	number = 4;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+
+	number = 5;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+
+	number = 6;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+
+	number = 7;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+
+	number = 8;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+
+	number = 9;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+
+	number = 10;
+	System.out.printf("%5s %5s %5s%n",  (number), (number * number), (number * number * number));
+}
+
+}
